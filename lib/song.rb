c44684ca8facbcbbd38e1ac339933f1ac6a9f2bf
Lena Hou <lenahou2013@gmail.com>
2021-02-10 15:58:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,20 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  @@count = 0
+  @@artists = 0
+  @@genres = 0
+
+  def self.count
+    @@count
+  end
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+
+    @@count += 1
   end
 
   @@count = 0
